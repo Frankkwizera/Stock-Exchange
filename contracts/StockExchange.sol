f68d750d1e59be5dd40d118180105566de9a0004
Frank <frankpmn@gmail.com>
2018-08-20 15:18:12 +0200

    Add files via upload
    first commit

---
+++ contracts/StockExchange.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+
+contract StockExchange {
+
+  mapping( address => uint ) balances;
+  uint holdings;
+
+  event Transaction(string _type, address indexed user);
+
+  //constructor to initialize holdings
+  function StockExchange(){
+    holdings = 10000;
+  }
+
+  function buy() payable{
+    if(msg.value == 1 ether){
+      balances[msg.sender] += 1;
+      holdings -=1;
+      Transaction("Buy",msg.sender);
+    }
+  }
+
+  function sell(){
+    balances[msg.sender] -=1;
+    holdings +=1;
+    msg.sender.transfer(1 ether);
+    Transaction("Sell",msg.sender);
+  }
+
+  function getBalance(address addr) returns(uint){
+    return balances[addr];
+  }
+
+  function getHoldings() returns(uint){
+
+    return holdings;
+  }
+}
